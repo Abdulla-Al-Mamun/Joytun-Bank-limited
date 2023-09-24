6ab9c6522f06b25dd8d133c187384e647239205f
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2023-09-24 22:44:11 +0600

    add js file and password and email massing using js

---
+++ log.js
@@ -0,0 +1,18 @@
+document.getElementById("btn-log").addEventListener('click', function () {
+    // get email 
+    const emailField = document.getElementById('field-email');
+    const email = emailField.value;
+    // get password
+    const passField = document.getElementById('field-pass');
+    const pass = passField.value;
+    // console.log(email, pass);
+
+    //  chack password and email 
+
+    if (email === 'mamun@gmail.com' && pass === 'mamun') {
+        console.log('valid user');
+    }
+    else {
+        console.log('invalid user');
+    }
+})
