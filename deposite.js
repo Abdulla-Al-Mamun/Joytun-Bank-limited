a9e8cd122c2a208b6639e05a2d9ec1db8e305e6a
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2023-09-27 21:19:07 +0600

    deposite field js

---
+++ deposite.js
@@ -0,0 +1,7 @@
+document.getElementById('btn-deposite').addEventListener('click', function () {
+
+    const depositeField = document.getElementById('deposite-field');
+    const depositeFieldString = depositeField.value;
+    const deposite = parseFloat(depositeFieldString);
+
+})
