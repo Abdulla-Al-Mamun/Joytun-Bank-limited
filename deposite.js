3ed193692f2431776a08e89278a970605ce6f550
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2023-09-27 23:18:31 +0600

    add deposite and total blance calculate

--- deposite.js
+++ deposite.js
@@ -1,7 +1,34 @@
+
+
+
 document.getElementById('btn-deposite').addEventListener('click', function () {
 
     const depositeField = document.getElementById('deposite-field');
     const depositeFieldString = depositeField.value;
-    const deposite = parseFloat(depositeFieldString);
+    const currentDeposite = parseFloat(depositeFieldString);
+
+    const deposite = document.getElementById('total-deposite');
+    const totalDepositeString = deposite.innerText;
+    const totalDeposite = parseFloat(totalDepositeString);
+
+    const totalDepositeCalculation = currentDeposite + totalDeposite;
+
+    deposite.innerText = totalDepositeCalculation;
+
+    const blance = document.getElementById('total-blance');
+    const blanceStrion = blance.innerText;
+    const currentBlance = parseFloat(blanceStrion);
+
+    const totalBlanceCalculation = currentBlance + currentDeposite;
+    blance.innerText = totalBlanceCalculation;
+
+    depositeField.value = '';
+
+
+
+
+
+
 
 })
+
