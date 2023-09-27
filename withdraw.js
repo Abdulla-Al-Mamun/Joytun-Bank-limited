c1142fa38088ec557dcc9858b5fb5b1a4dbf5523
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2023-09-27 23:31:45 +0600

    total withdraw calculation

--- withdraw.js
+++ withdraw.js
@@ -1,1 +1,18 @@
-console.log('withdraw btn clicked');
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+    const withdrawField = document.getElementById('withdraw-field');
+    const withdrawString = withdrawField.value;
+    const currentWithdraw = parseFloat(withdrawString);
+
+    const withdraw = document.getElementById('total-withdraw');
+
+    const totalWithdrawString = withdraw.innerText;
+    const currentTotalWithdraw = parseFloat(totalWithdrawString);
+
+    const totalWithdrawCalculation = currentTotalWithdraw + currentWithdraw;
+    withdraw.innerText = totalWithdrawCalculation;
+
+
+
+    withdrawField.value = '';
+
+})
